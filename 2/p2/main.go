29c34fd3133e742c11639d25de431a1cd60d3591
Anurag <anuragp.5984@gmail.com>
2024-12-02 22:29:45 +0530

    day 2

--- 2/p2/main.go
+++ 2/p2/main.go
@@ -28,29 +28,31 @@ func main() {
 	data := parse_input(string(content))
 
 	total := 0
-	for i := 0; i < len(data); i++ {
+	for i := range data {
 		if is_safe(data[i]) {
 			total += 1
 		}
 	}
 
-	// SOLLUTION: 334
+	// SOLLUTION: 400
 	fmt.Println("TOTAL:", total)
 }
 
 func is_safe(slice []int) bool {
-	prev := slice[0]
+	if is_sorted(slice) {
+		return gol_d_lock(slice)
+	} else {
+		for i := range slice {
+			temp := make([]int, 0, len(slice)-1)
+			temp = append(temp, slice[:i]...)
+			temp = append(temp, slice[i+1:]...)
 
-	sorted := is_sorted(slice)
-	for i := 1; i < len(slice); i++ {
-		diff := math.Abs(float64(slice[i] - prev))
-		if sorted && prev != slice[i] && diff <= 3 {
-			prev = slice[i]
-		} else {
-			return false
+			if is_sorted(temp) {
+				return gol_d_lock(temp)
+			}
 		}
 	}
-	return true
+	return false
 }
 
 func is_sorted(slice []int) bool {
@@ -58,15 +60,29 @@ func is_sorted(slice []int) bool {
 	descending := true
 
 	for i := 0; i < len(slice)-1; i++ {
-		if slice[i] > slice[i+1] {
+		if slice[i] >= slice[i+1] {
 			ascending = false
 		}
-		if slice[i] < slice[i+1] {
+		if slice[i] <= slice[i+1] {
 			descending = false
 		}
 	}
 
 	return ascending || descending
+}
+
+func gol_d_lock(slice []int) bool {
+	prev := slice[0]
+
+	for i := range slice {
+		diff := math.Abs(float64(slice[i] - prev))
+		if diff <= 3 {
+			prev = slice[i]
+		} else {
+			return false
+		}
+	}
+	return true
 }
 
 func parse_input(input string) [][]int {
