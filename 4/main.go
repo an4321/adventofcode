8ab1fd5ec7092c3e81c0d9cb060e487cb0088293
Anurag <anuragp.5984@gmail.com>
2024-12-04 17:33:32 +0530

    day 4 part 2

--- 4/main.go
+++ 4/main.go
@@ -30,6 +30,7 @@ func main() {
 
 	// Part 1: 2583
 	fmt.Println("PART 1:", part1(matrix))
+	fmt.Println("PART 2:", part2(matrix))
 }
 
 func part1(matrix [][]rune) int {
@@ -83,6 +84,27 @@ func part1(matrix [][]rune) int {
 				// South West
 				if len(matrix)-i > 3 && len(matrix[i][:j])+1 > 3 {
 					if matrix[i+1][j-1] == 'M' && matrix[i+2][j-2] == 'A' && matrix[i+3][j-3] == 'S' {
+						total++
+					}
+				}
+			}
+		}
+	}
+	return total
+}
+
+func part2(matrix [][]rune) int {
+	total := 0
+
+	for i := range matrix {
+		for j := range matrix[i] {
+			if matrix[i][j] == 'A' {
+				if i > 0 && j > 0 && i+1 < len(matrix) && j+1 < len(matrix[i]) {
+					// Not proud of this.
+					if (matrix[i-1][j-1] == 'M' && matrix[i+1][j-1] == 'M' && matrix[i-1][j+1] == 'S' && matrix[i+1][j+1] == 'S') ||
+						(matrix[i-1][j-1] == 'M' && matrix[i+1][j-1] == 'S' && matrix[i-1][j+1] == 'M' && matrix[i+1][j+1] == 'S') ||
+						(matrix[i-1][j-1] == 'S' && matrix[i+1][j-1] == 'M' && matrix[i-1][j+1] == 'S' && matrix[i+1][j+1] == 'M') ||
+						(matrix[i-1][j-1] == 'S' && matrix[i+1][j-1] == 'S' && matrix[i-1][j+1] == 'M' && matrix[i+1][j+1] == 'M') {
 						total++
 					}
 				}
