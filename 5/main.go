a62c28b62d6ce1de31c96e3153de1f8df391297c
Anurag <anuragp.5984@gmail.com>
2024-12-05 14:08:48 +0530

    day 4 almost

--- 5/main.go
+++ 5/main.go
@@ -18,47 +18,79 @@ func main() {
 	rules := parse_input(strings.ReplaceAll(string(rules_content), "|", " "))
 	data := parse_input(strings.ReplaceAll(string(input_content), ",", " "))
 
-	// rules = [][]int{
-	// 	{47, 53}, {97, 13}, {97, 61}, {97, 47}, {75, 29}, {61, 13}, {75, 53},
-	// 	{29, 13}, {97, 29}, {53, 29}, {61, 53}, {97, 53}, {61, 29}, {47, 13},
-	// 	{75, 47}, {97, 75}, {47, 61}, {75, 61}, {47, 29}, {75, 13}, {53, 13},
-	// }
+	rules = [][]int{
+		{47, 53}, {97, 13}, {97, 61}, {97, 47}, {75, 29}, {61, 13}, {75, 53},
+		{29, 13}, {97, 29}, {53, 29}, {61, 53}, {97, 53}, {61, 29}, {47, 13},
+		{75, 47}, {97, 75}, {47, 61}, {75, 61}, {47, 29}, {75, 13}, {53, 13},
+	}
+	data = [][]int{
+		{75, 47, 61, 53, 29},
+		{97, 61, 53, 29, 13},
+		{75, 29, 13},
+		{75, 97, 47, 61, 53},
+		{61, 13, 29},
+		{97, 13, 75, 29, 47},
+	}
 
-	// data = [][]int{
-	// 	{75, 47, 61, 53, 29},
-	// 	{97, 61, 53, 29, 13},
-	// 	{75, 29, 13},
-	// 	{75, 97, 47, 61, 53},
-	// 	{61, 13, 29},
-	// 	{97, 13, 75, 29, 47},
-	// }
-
-    // Sol: 4637
+	// Sol: 4637
 	fmt.Println("Part1:", part1(rules, data))
+	// Sol: 11007> 
+	fmt.Println("Part2:", part2(rules, data))
 }
 
 func part1(rules [][]int, data [][]int) int {
 	total := 0
 	for i := range data {
-		safe := true
-
-		for j := range data[i] {
-			for k := range rules {
-				if rules[k][1] == data[i][j] {
-					for l := range data[i][j+1:] {
-						if data[i][j+1+l] == rules[k][0] {
-							safe = false
-						}
-					}
-				}
-			}
-		}
-		if safe {
+		if is_safe(rules, data[i]) {
 			total += data[i][len(data[i])/2]
 		}
 	}
 
 	return total
+}
+
+func part2(rules [][]int, data [][]int) int {
+	total := 0
+	for i := range data {
+		if !is_safe(rules, data[i]) {
+			for {
+				for j := range data[i] {
+					for k := range rules {
+						if rules[k][1] == data[i][j] {
+							for l := range data[i][j+1:] {
+								if data[i][j+1+l] == rules[k][0] {
+									temp := data[i][j]
+									data[i][j] = data[i][j+1+l]
+									data[i][j+1+l] = temp
+								}
+							}
+						}
+					}
+				}
+				if is_safe(rules, data[i]) {
+					break
+				}
+			}
+		}
+		total += data[i][len(data[i])/2]
+	}
+	return total
+}
+
+func is_safe(rules [][]int, slice []int) bool {
+	for i := range slice {
+		for j := range rules {
+			if rules[j][1] == slice[i] {
+				for k := range slice[i+1:] {
+					if slice[i+1+k] == rules[j][0] {
+						return false
+					}
+				}
+			}
+		}
+	}
+
+	return true
 }
 
 func parse_input(input string) [][]int {
